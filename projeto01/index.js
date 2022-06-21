c6541dd0e206f92815570c7d2a650d7f6a51b2d1
samuel dias <samuel.dias36@etec.sp.gov.br>
2022-06-21 13:00:20 -0300

    inicio javascript

---
+++ projeto01/index.js
@@ -0,0 +1,56 @@
+// var olamundo = "Olá terra";
+
+//o javascript possui variaveis. As variaveis são declaradas com o comando var. elas facilitam na hora de manipular os dados repetitivos. 
+
+//há dois tipos de comandos que tambem se usa para declarar variaveis : o let e o const. o let é um comando que permite a declaração de variaveis com o escopo de bloco, ou seja, seu valor é apenas válido dentro do bloco. o const é um comando que permite a declaração de constante com o escopo de bloco, ou seja, seu valor é apenas válido dentro do bloco.const é const e var é var.
+
+//variaveis podem guardar qualquer tipo de dado. string, number, boolean, object, array, function, etc. o typeof é um comando que retorna o tipo de dado que está guardado na variavel. o stanceof é um comando que mostra de que instancia veio a variavel. 
+
+
+/*console.log(olamundo); 
+console.log(olamundo);
+console.log(olamundo);
+*/
+
+
+       //alert("Olá Mundo"); tanto faz abrir com as aspas simples ou duplas. esse console.log é para testar o código. No javascript o console.log é um comando que mostra algo na tela. É case-sensitive, ou seja, é necessário colocar o console.log em caixa alta  ou baixa para que o console.log funcione. Este é uma forma de script incorporado. Por que ? porque está junto com o código html. Não é a melhor solução. 
+
+       //operador ==
+       
+      /* let a=10;
+       const b=20; */
+
+      // console.log(a==b);
+       
+      //operador de comparação, vai retornar true ou false.
+
+//operador ===
+ 
+
+/*      let a=10;
+       const b="10";
+
+       console.log(a===b);
+
+       */
+
+       //temos 3 tipos de igual : igual simples = igual de atribuição ; igual duplo= comparação de valor ; igual triplo = comparação de valor e igualdade de dado. 
+
+   /*    let a=10;
+       const b="10";
+
+       console.log(a!==b);
+
+       */
+
+
+       //para retornar a resposta diferente é necessario colocar o ! antes do operador. Se o ! for sucedido por dois iguais ele vai comparar o valor e o tipo. Se o ! for sucedido por um igual ele vai comparar o valor.
+
+      //operadores logicos.  
+       
+       let a=10;
+       const b="10";
+
+       console.log(a==b && typeof b == "string");
+
+       
